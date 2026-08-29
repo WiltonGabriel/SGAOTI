e89e2db4e5b0a1a13f75ca5f5199a4547d2b863b
Wilton Gabriel <wiltongabriel81@gmail.com>
2026-08-29 18:04:17 -0400

    feat: Update Ativo class to include additional properties and constructor parameters for enhanced asset representation

--- src/model/Ativo.java
+++ src/model/Ativo.java
@@ -1,14 +1,30 @@
 package model;
 
-public class Ativo {
-    private String patrimonio;
-    private String descricao;
-    private String status;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-    public Ativo(String patrimonio, String descricao, String status) {
+public class Ativo {
+    private String nome;
+    private LocalDateTime dataRegistro;
+
+    private String patrimonio; //OPT
+    private String descricao; //OPT
+    private String padraoEncaixe; //IF CPU/Mobo/RAM
+
+    private String status; //Funcionando/Defeito$/Desconhecido/Inoperante default: Pendente
+
+    private String categoria;
+    private ArrayList<Ativo> componentes;
+
+    public Ativo(String nome, String patrimonio, String descricao, String status, String padraoEncaixe, String categoria) {
+        this.nome = nome;
+        this.dataRegistro = LocalDateTime.now();
         this.patrimonio = patrimonio;
         this.descricao = descricao;
+        this.padraoEncaixe = padraoEncaixe;
         this.status = status;
+        this.categoria = categoria;
+        this.componentes = new ArrayList<>();
     }
 
     public String getPatrimonio() {
@@ -19,8 +35,28 @@ public class Ativo {
         return descricao;
     }
 
+    public String getPadraoEncaixe() {
+        return padraoEncaixe;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDateTime getDataRegistro() {
+        return dataRegistro;
+    }
+
     public String getStatus() {
         return status;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public ArrayList<Ativo> getComponentes() {
+        return componentes;
     }
 
 }
